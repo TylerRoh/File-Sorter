1aeea4c700ae6c7af479e3af137e860b95448451
trohweder <tyler.s.rohweder@gmail.com>
2020-03-30 10:06:47 -0500

    Innitial commit

---
+++ File Sorter/File Sorter/File Sorter/DirectoryCheck.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace File_Sorter
+{
+    public class DirectoryCheck
+    {
+        
+        public static bool checkDirectories(string path)
+        {
+            var neededDirectories = new List<string>() {"PDF","EXE","EXCEL","DOCUMENTS","ZIP"};
+            Console.WriteLine("The following directories are needed: ");
+            foreach (var directory in neededDirectories)
+            {
+                if (Directory.Exists(path + directory))
+                {
+                    continue;
+                }
+                Console.WriteLine("Need to creat directory {0}", directory);
+                
+            }
+            Console.WriteLine("To create needed directories and sort files enter \"yes\".");
+            var answer = Console.ReadLine().ToLower().Trim();
+            if (answer == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void createDirectories(string path)
+        {
+            var neededDirectories = new List<string>() { "PDF", "EXE", "EXCEL", "DOCUMENTS", "ZIP", "MISC" };
+            foreach (var directory in neededDirectories)
+            {
+                if (Directory.Exists(path + directory))
+                {
+                    continue;
+                }
+                Directory.CreateDirectory(path + directory);
+                Console.WriteLine("{0} created", directory);
+            }
+        }
+        public static void moveFiles(string[] files, string path)
+        {
+
+            foreach(var file in files)
+            {
+                var extension = Path.GetExtension(file);
+                if (extension == ".exe")
+                    File.Move(file, path + @"EXE\" + Path.GetFileName(file), true);
+                else if (extension == ".pdf")
+                    File.Move(file, path + @"PDF\" + Path.GetFileName(file), true);
+                else if (extension == ".xlsx" || extension == ".csv" || extension == ".xls")
+                    File.Move(file, path + @"EXCEL\" + Path.GetFileName(file), true);
+                else if (extension == ".docx" || extension == ".doc")
+                    File.Move(file, path + @"DOCUMENTS\" + Path.GetFileName(file), true);
+                else if (extension == ".zip")
+                    File.Move(file, path + @"ZIP\" + Path.GetFileName(file), true);
+                else
+                    File.Move(file, path + @"MISC\" + Path.GetFileName(file), true);
+            }
+            
+        }
+    }
+}
---
+++ File Sorter/File Sorter/File Sorter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+namespace File_Sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Path to downloads folder goes here
+            var path = @"C:\Users\trohweder\Downloads\";
+
+            if (DirectoryCheck.checkDirectories(path))
+            {
+                DirectoryCheck.createDirectories(path);
+            }
+            var files = Directory.GetFiles(path);
+            DirectoryCheck.moveFiles(files, path);
+
+        }
+    }
+}
